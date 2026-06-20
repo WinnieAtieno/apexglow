32424a4c5d8dccadb78cfd883c24227098c096b5
WinnieAtieno <winnieotieno773@gmail.com>
2026-06-20 23:06:25 +0300

    completed nav routes

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -1,7 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/Home"
+import Home from "../pages/Home";
+import How from "../pages/How"
+import Contact from "../pages/Contact"
+import Services from "../pages/Services"
 import Login from "../pages/Login"
 import Layout from "../components/Layout";
+import Packages from "../pages/Packages";
 
 
 
@@ -13,6 +17,22 @@ export const router= createBrowserRouter([
         {
             index:true,
             element:<Home/>
+        },
+        {
+            path:"/packages",
+            element:<Packages/>
+        },
+        {
+            path:"/how",
+            element:<How/>
+        },
+        {
+            path:"/services",
+            element:<Login/>
+        },
+        {
+            path:"/contact",
+            element:<Contact/>
         },
         {
             path:"/login",
